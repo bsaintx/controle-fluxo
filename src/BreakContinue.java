e2f00867582f43e1be124bfe42dde03a63bc76b1
bsaintx <santos.bruno.coutinho@gmail.com>
2023-09-06 16:17:17 -0300

    Break e Continue

---
+++ src/BreakContinue.java
@@ -0,0 +1,22 @@
+public class BreakContinue {
+    public static void main(String[] args) {
+        System.out.println("  ");
+        System.out.println("Break");
+        for(int numero = 1; numero <= 5; numero ++) {
+            if (numero == 3) {
+                break; // Interrompe o laço
+            }
+
+            System.out.println(numero);
+        }
+        System.out.println("  ");
+        System.out.println("Continue");
+        for(int par = 0; par <= 20; par ++) {
+            if (par % 2 != 0) { // número par ---- ímpar: % 2 != 0
+                continue; // Pula um valor específico
+            }
+            
+            System.out.println(par);
+        }
+    }
+}
