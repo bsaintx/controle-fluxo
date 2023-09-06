75c27cdc397f8782fc961d657f10662702f5543b
bsaintx <santos.bruno.coutinho@gmail.com>
2023-09-06 15:28:22 -0300

    For e ForEach - Arrays

---
+++ src/ForArrays.java
@@ -0,0 +1,20 @@
+public class ForArrays {
+    public static void main(String[] args) {
+
+        System.out.println("  ");
+        System.out.println("For Clássico");
+        
+        String alunos [] = { "Bruno", "Juliana", "Nathalia", "Maycon"};
+
+        for (int x = 0; x < alunos.length; x++) {
+            System.out.println("O aluno no índice x= " + x + " é " + alunos[x]);
+        }
+
+        System.out.println("  ");
+        System.out.println("ForEach");
+
+        for (String aluno : alunos) {
+            System.out.println("Nome do aluno: " + aluno);
+        }
+    }
+}
