721a6a9e5d1b1e119be730f6f6813b119426fd2c
bsaintx <santos.bruno.coutinho@gmail.com>
2023-09-07 23:04:12 -0300

    4. Estrutura Condicional Ternária

--- src/NotaEscolar.java
+++ src/NotaEscolar.java
@@ -1,6 +1,6 @@
 public class NotaEscolar {
     public static void main(String[] args) {
-        int nota = 4;
+        int nota = 6;
 
         String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
         System.out.println(resultado);
